7696ee6ac8f1cf00a4247224bfb3bb6f99cc1e7a
iwWh1te <ylanovru10@gmail.com>
2023-05-27 15:06:45 +0400

    3_incr

--- test.js
+++ test.js
@@ -1,7 +1,30 @@
 //test.js
 
-
+var cnt=0;
 var posArray=[49,50,48,51,52];
-function onClickShowItem() {
-	alert(posArray);
+var cntStep=1;
+var cntMin=0;
+var cntMax=9;
+var textWarning="\n";
+
+function onClickBack() {
+	cnt-=cntStep;
+	if(cnt<cntMin){
+		cnt=cntMin;
+		textWarning=textWarning+"Reached the lower limit of array";
+	}
+	alert("Clicked button : Back"+"\n"+"cnt = "+cnt+textWarning);
+	textWarning="\n";
+	return false;
+}
+
+function onClickNext() {
+	cnt+=cntStep;
+	if(cnt>cntMax){
+		cnt=cntMax;
+		textWarning=textWarning+"Reached the upper limit of array";
+	}
+	alert("Clicked button : Next"+"\n"+"cnt = "+cnt+textWarning);
+	textWarning="\n";
+	return false;
 }
